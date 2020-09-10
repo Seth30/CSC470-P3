16ebb53483ddfa9a972dc2edbd336b870fb83b55
Seth Huot <Seth.Huot@trojans.dsu.edu>
2020-09-10 14:07:48 -0500

    created AppUser class and added attributes

---
+++ P3 Code/P3 Code/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace P3_Code
+{
+    public class AppUser
+    {
+        public string UserName;
+        public string Password;
+        public string FirstName;
+        public string LastName;
+        public string EmailAddress;
+        public bool IsAuthenticated;
+    }
+}
--- P3 Code/P3 Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace P3_Code
-{
-    public class Class1
-    {
-    }
-}
