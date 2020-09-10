f3059470bfa6ce2e4d6230f810a2d7012d2708bb
Seth Huot <Seth.Huot@trojans.dsu.edu>
2020-09-10 14:17:34 -0500

    Made Interface and created 4 methods

---
+++ P3 Code/P3 Code/IAppUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P3_Code
+{
+    interface IAppUser
+    {
+        bool Login(string UserName, string Password);
+
+        List<AppUser> GetAll();
+
+        void SetAuthentication(string UserName, bool IsAuthenticated);
+
+        AppUser GetByUserName(string UserName);
+    }
+}
