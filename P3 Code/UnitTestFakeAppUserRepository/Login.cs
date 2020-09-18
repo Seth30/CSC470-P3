90edabe7a44890500fa3e7f47cb2b3350241f1a3
bhimr <35665192+bhimr@users.noreply.github.com>
2020-09-17 23:35:27 -0500

    This is my part done

---
+++ P3 Code/UnitTestFakeAppUserRepository/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using P3_Code;
+
+namespace UnitTestFakeAppUserRepository
+{
+    [TestClass]
+    public class Login
+    {
+        [TestMethod]
+        public void LoginWithCorrectCredentialsSuccessful()
+        {
+
+            const string UserName = "DaveBish";
+            const string PassWord = "iLoveSoftware";
+            FakeAppUserRepository USRepository = new FakeAppUserRepository();
+
+            // Act
+            bool result = USRepository.Login(UserName,PassWord);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+        public void LoginWithIncorrectCredentialsFail()
+        {
+            // Arrange
+            const string UserName = "test";
+            const string PassWord = "fail";
+            FakeAppUserRepository USRepository = new FakeAppUserRepository();
+
+            // Act
+            bool result = USRepository.Login(UserName, PassWord);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ P3 Code/UnitTestFakeAppUserRepository/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestFakeAppUserRepository")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestFakeAppUserRepository")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af45484c-dd96-483d-af26-fada100635ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
